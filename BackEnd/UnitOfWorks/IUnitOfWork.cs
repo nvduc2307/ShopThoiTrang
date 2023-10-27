748696a3f2d57d234af54cbf984c4295f7185cd4
nvduc2307 <nvduc.yt@gmail.com>
2023-10-27 09:28:55 +0700

    create-front-end-mvc

--- BackEnd/UnitOfWorks/IUnitOfWork.cs
+++
@@ -1,8 +0,0 @@
-using ShopThoiTrang.BackEnd.IRepositories;
-
-namespace ShopThoiTrang.BackEnd.UnitOfWorks;
-
-public interface IUnitOfWork {
-    IProductRepository IProductRepository{get;}
-    int Save();
-}
