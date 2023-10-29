9d2ff372a15bb9193a878c421a62bd9113bb27f0
apple <apple@apples-MacBook-Pro.local>
2023-10-29 22:27:57 +0700

    config-admin-layout

---
+++ FrontEnd/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers;
+public class AdminController : Controller {
+    private readonly ILogger<AdminController> _logger;
+
+    public AdminController(ILogger<AdminController> logger)
+    {
+        _logger = logger;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
